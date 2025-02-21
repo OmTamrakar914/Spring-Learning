9c59a8155af6c882b79b37dd594e4137c593f05e
OmTamrakar914 <omtamrakar9143@gmail.com>
2025-02-21 16:27:55 +0530

    [Om] Add. Completed UC1-printing messege in console

---
+++ UC1_ProjectUsingIntellj/src/main/java/com/example/UC1_projectusingintellj/Uc1ProjectUsingInitialzrApplication.java
@@ -0,0 +1,15 @@
+package com.example.UC1_projectusingintellj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Uc1ProjectUsingInitialzrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Uc1ProjectUsingInitialzrApplication.class, args);
+		System.out.println("Hello World!!");
+	}
+
+
+}
---
+++ UC1_ProjectUsingIntellj/src/test/java/com/example/UC1_projectusinginitialzr/Uc1ProjectUsingInitialzrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UC1_projectusinginitialzr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Uc1ProjectUsingInitialzrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
